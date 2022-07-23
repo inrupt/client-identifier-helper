1fc55dd10a67e2a2c8fc37aa68203648f14e889a
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-07-23 20:04:09 +0200

    style: fix eslint errors & formatting

--- src/clientIdGenerator/index.ts
+++ src/clientIdGenerator/index.ts
@@ -18,20 +18,3 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-
-import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
-
-import theme from "./theme";
-import ClientIdentifierGenerator from "./components/ClientIdentifierGenerator";
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <ClientIdentifierGenerator />
-    </ThemeProvider>
-  );
-}
-
-export default App;
