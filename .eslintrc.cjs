b72091c014c8268b3cd9fbdd66f59189bbc8d0e8
Laurin-W <59744144+Laurin-W@users.noreply.github.com>
2022-08-01 14:42:36 +0200

    feat: initial setup (#1)
    initial project setup (vite, react, eslint)

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+
+  extends: [
+    "@inrupt/eslint-config-react",
+    "@inrupt/eslint-config-lib",
+    "plugin:import/typescript",
+  ],
+
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+
+  // These settings and the `plugin:import/typescript` are required until we add
+  // this configuration to our @inrupt/eslint-config-lib base
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.eslint.json",
+      },
+    },
+  },
+
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"],
+      },
+    ],
+  },
+};
