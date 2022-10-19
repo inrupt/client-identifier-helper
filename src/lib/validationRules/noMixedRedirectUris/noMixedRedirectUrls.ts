81b6eabeda8b46519ee57062a77cb0f5613189e3
Laurin Weger <Laurin-W@users.noreply.github.com>
2022-10-19 23:29:03 +0200

    remove linting warning

--- src/lib/validationRules/noMixedRedirectUris/noMixedRedirectUrls.ts
+++ src/lib/validationRules/noMixedRedirectUris/noMixedRedirectUrls.ts
@@ -18,7 +18,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-import { isHostnameLocal, isUriLocalhost } from "../../helperFunctions";
+import { isUriLocalhost } from "../../helperFunctions";
 import { ValidationContext, ValidationRule } from "../../types";
 
 const noMixedRedirectUrls: ValidationRule = {
